162c037f2a3c024cd2d0d778d73e804d3e95bfc6
temperancemonkey <downt872@newschool.edu>
2025-03-06 09:58:53 -0500

    class workshop mar 6

--- studio/workshops/penny-presser/scripts.js
+++ studio/workshops/penny-presser/scripts.js
@@ -1,5 +1,6 @@
 // get the press button
 var pressButton = document.querySelector('button[name="press"]');
+var studentSelect = document.querySelector('select[name="student"]');
 var pennySelect = document.querySelector('select[name="penny"]');
 var pennyImage = document.querySelector('.penny');
 
@@ -14,9 +15,17 @@ pressButton.addEventListener('click', () => {
     console.log(pennySelect.value);
     console.log(pennyImage);
 
+// log the student value
+
+console.log(studentSelect.value);
+
     // set the penny image src
-    pennyImage.src = 'img/' + pennySelect.value;
+    // pennyImage.src = 'img/' + pennySelect.value;
+
+    pennyImage.src = 'https://' + studentSelect.value + '.interactive.rodeo/studio/workshops/penny-presser/img/' + pennySelect.value;
 });
+
+
 
 
 
