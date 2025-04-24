dff172ac1864a414e28eddbf2ca56aed0b0769a5
temperancemonkey <downt872@newschool.edu>
2025-04-24 11:15:33 -0400

    .

---
+++ studio/projects/thinkitsandtrinkets_projectthree/script.js
@@ -0,0 +1,11 @@
+// get all buttons button
+
+var buttons = document.querySelector('button');
+
+//get organize button
+
+var organize = document.querySelector('organize');
+
+//add eventlistener
+
+
